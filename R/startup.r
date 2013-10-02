57fc73e84de7416f254aea51f3593355658bf56e
schamberlain <myrmecocystus@gmail.com>
2013-10-02 11:22:24 -0700

    cowsay startup message

--- R/startup.r
+++ R/startup.r
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-	packageStartupMessage("\n\n NOAA web services are not available due to the Gov't shut down...check back later\n")
+	packageStartupMessage("\n ----- \n NOAA web services are not available \n due to the Gov't shut down...check back later\n ------ \n    \\\   ^__^ \n     \\\  (oo)\\\ ________ \n        (__)\\\         )\\\ /\\\ \n             ||------w|\n             ||      ||")
 } 
