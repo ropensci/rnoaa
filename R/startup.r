d903a2a231720db8ab98dc4c69061d52f909d0b1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-21 10:25:57 -0700

    updated description file and updated startup fxn

--- R/startup.r
+++ R/startup.r
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-	packageStartupMessage("\n\n New to rnoaa? Tutorial at http://ropensci.org/tutorials/rnoaa_tutorial.html. \n A new NOAA API (v2) is out. Most functions have changed. \n Use suppressPackageStartupMessages() to suppress these startup messages in the future\n")
+	packageStartupMessage("\n\n New to rnoaa? See package vignettes, and see the tutorial at http://ropensci.org/tutorials/rnoaa_tutorial.html. \n A new NOAA API (v2) is out. Most functions have changed. \n Use suppressPackageStartupMessages() to suppress these startup messages in the future\n")
 } 
