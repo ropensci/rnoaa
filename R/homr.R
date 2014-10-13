3fe925f6b9f72bacc4354db9f5fea1884c06d975
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-13 14:10:43 -0700

    finished off homr fxns, closes #59

--- R/homr.R
+++ R/homr.R
@@ -4,40 +4,47 @@
 #'
 #' @param qid One of COOP, FAA, GHCND, ICAO, NCDCSTNID, NWSLI, TRANS, WBAN, or WMO, or any
 #' of those plus \code{[a-z0-9]}, or just \code{[a-z0-9]}.
-#' @param qidMod One of is, starts, ends, contains. Specifies how the ID portion of the qid
-#' parameter should be applied within the search. If a qid is passed but the qidMod parameter
-#' is not used, qidMod is assumed to be IS.
-#' @param state A two-letter state abbreviation. Two-letter code for US states, Canadian
-#' provinces, and other Island areas.
-#' @param county A two letter county code. US county names, best used with a state identifier.
-#' @param country A two letter country code. See here for a list of valid country names.
-#' @param name One of name=[0-9A-Z]+. Searches on any type of name we have for the station.
-#' @param nameMod [is|starts|ends|contains]. Specifies how the name parameter should be applied
-#' within the search. If a name is passed but the nameMod parameter is not used, nameMod is assumed
-#' to be IS.
-#' @param platform (aka network) [ASOS|USCRN|USHCN|NEXRAD|AL USRCRN|USRCRN|COOP]. Limit the
-#' search to stations of a certain platform/network type.
-#' @param date [YYYY-MM-DD|all] Limits values to only those that occurred on a specific date.
-#' Alternatively, date=all will return all values for matched stations. If this field is omitted,
-#' the search will return only the most recent values for each field.
+#' @param qidMod  (character) One of: is, starts, ends, contains. Specifies how the ID portion of 
+#' the qid parameter should be applied within the search. If a qid is passed but the qidMod 
+#' parameter is not used, qidMod is assumed to be IS.
+#' @param station  (character) A station id. 
+#' @param state  (character) A two-letter state abbreviation. Two-letter code for US states, 
+#' Canadian provinces, and other Island areas.
+#' @param county  (character) A two letter county code. US county names, best used with a state 
+#' identifier.
+#' @param country  (character) A two letter country code. See here for a list of valid country 
+#' names.
+#' @param name  (character) One of name=[0-9A-Z]+. Searches on any type of name we have for the 
+#' station.
+#' @param nameMod  (character) [is|starts|ends|contains]. Specifies how the name parameter should 
+#' be applied within the search. If a name is passed but the nameMod parameter is not used, 
+#' nameMod is assumed to be IS.
+#' @param platform  (character) (aka network) [ASOS|USCRN|USHCN|NEXRAD|AL USRCRN|USRCRN|COOP]. 
+#' Limit the search to stations of a certain platform/network type.
+#' @param date  (character) [YYYY-MM-DD|all] Limits values to only those that occurred on a 
+#' specific date. Alternatively, date=all will return all values for matched stations. If this 
+#' field is omitted, the search will return only the most recent values for each field.
 #' @param begindate,enddate [YYYY-MM-DD]. Limits values to only those that occurred within a
 #' date range.
-#' @param headersOnly true Returns only minimal information for each station found (NCDC Station
-#' ID, Preferred Name, Station Begin Date, and Station End Date), but is much quicker than a
+#' @param headersOnly (logical) Returns only minimal information for each station found (NCDC 
+#' Station ID, Preferred Name, Station Begin Date, and Station End Date), but is much quicker than a
 #' full query. If you are performing a search that returns a large number of stations and intend
 #' to choose only one from that list to examine in detail, headersOnly may give you enough
 #' information to find the NCDC Station ID for the station that you actually want.
-#' @param phrData false The HOMR web service now includes PHR (element-level) data when
+#' @param phrData (logical) The HOMR web service now includes PHR (element-level) data when
 #' available, in an elements section. Because of how this data is structured, it can substantially
 #' increase the size of any result which includes it. If you don't need this data you can omit it
 #' by including phrData=false. If the parameter is not set, it will default to phrData=true.
-#' @param definitions false The HOMR web service includes a list of definitions with every result.
-#' These are meant to serve as a reference for some of the more cryptic variable values, and are
-#' identical for every request. You can omit these from your result by setting 
-#' \code{definitions=FALSE}.
+#' @param combine (logical) Combine station metadata or not. 
 #' @param ... Further named parameters, such as \code{query}, \code{path}, etc, passed on to 
 #' \code{\link[httr]{modify_url}}. Unnamed parameters will be combined with 
 #' \code{\link[httr]{config}}. 
+#' 
+#' @details Since the definitions for variables are always the same, we don't include the ability
+#' to get description data in this function. Use \code{link[rnoaa]{homr_descriptions}} to get
+#' descriptions information.
+#' 
+#' @return A list, with elements named by the station ids.
 #'
 #' @examples \donttest{
 #' homr(qid = 'COOP:046742')
@@ -46,76 +53,69 @@
 #' homr(qid = 'FAA:')
 #' homr(qidMod='starts', qid='COOP:0467')
 #' homr(headersOnly=TRUE, state='DE')
-#' homr(headersOnly=TRUE, state='NC', county='BUNCOMBE')
 #' homr(headersOnly=TRUE, country='GHANA')
+#' homr(headersOnly=TRUE, state='NC', county='BUNCOMBE')
 #' homr(name='CLAYTON')
+#' res <- homr(name='CLAYTON', combine=TRUE)
+#' res$id
+#' res$head
+#' res$updates
 #' homr(nameMod='starts', name='CLAY')
 #' homr(headersOnly=TRUE, platform='ASOS')
 #' homr(qid='COOP:046742', date='2011-01-01')
 #' homr(qid='COOP:046742', begindate='2005-01-01', enddate='2011-01-01')
 #' homr(state='DE', headersOnly=TRUE)
+#' homr(station=20002078)
 #' homr(station=20002078, date='all', phrData=FALSE)
-#' homr(station=20002078, date='all', definitions=FALSE)
 #' }
 
-homr <- function(qid=NULL, qidMod=NULL, state=NULL, county=NULL, country=NULL, name=NULL,
-  nameMod=NULL, platform=NULL, date=NULL, begindate=NULL, enddate=NULL, headersOnly=NULL,
-  phrData=NULL, definitions=NULL, ...)
+homr <- function(qid=NULL, qidMod=NULL, station=NULL, state=NULL, county=NULL, country=NULL, 
+  name=NULL, nameMod=NULL, platform=NULL, date=NULL, begindate=NULL, enddate=NULL, headersOnly=FALSE,
+  phrData=NULL, combine=FALSE, ...)
 {
   args <- noaa_compact(list(qid=qid, qidMod=qidMod, state=state, county=county, country=country,
       name=name, nameMod=nameMod, platform=platform, date=date, begindate=begindate,
-      enddate=enddate, headersOnly=headersOnly, phrData=phrData, definitions=definitions))
-  res <- GET(homr_base(), query=args, ...)
+      enddate=enddate, headersOnly=tl(headersOnly), phrData=tl(phrData), definitions='false'))
+  url <- if(is.null(station)) paste0(homr_base(), "search") else paste0(homr_base(), station)
+  res <- GET(url, query=args, ...)
   out <- content(res, "text")
   json <- jsonlite::fromJSON(out, FALSE)
-  defs <- parse_defs(json$stationCollection$definitions)
-  sts <- 
-    lapply(json$stationCollection$stations, parse_stations)
-  structure(list(definition=defs, stations=sts), class="homr")
+  sts <- lapply(json$stationCollection$stations, parse_stations, headersOnly=headersOnly)
+  names(sts) <- sapply(sts, "[[", "id")
+  if(!combine) structure(sts, class="homr", combined=FALSE) else structure(combine_stations(sts), class="homr", combined=TRUE)
 }
 
-homr_base <- function() 'http://www.ncdc.noaa.gov/homr/services/station/search'
-
-parse_defs <- function(x){
-  do.call(rbind.fill, lapply(x, data.frame, stringsAsFactors = FALSE))
-}
-
-parse_stations <- function(x) {
+parse_stations <- function(x, headersOnly) {
   id <- x$ncdcStnId
   head <- data.frame(x$header, stringsAsFactors = FALSE)
-  namez <- rbf(todf(x$names))
-  identifiers <- rbf(todf(x$identifiers))
-  location <- parse_loc(x$location)
-  status <- x$statuses[[1]]$status
-  platform <- x$platforms[[1]]$platform
-  relocations <- todf(x$relocations)[[1]]
-  remarks <- rbf(todf(x$remarks))
-  updates <- todf(x$updates)[[1]]
-  elements <- rbf(todf(x$elements))
-  list(id=id, head=head, namez=namez, identifiers=identifiers, location=location, 
-       status=status, platform=platform, relocations=relocations, 
-       remarks=remarks, updates=updates, elements=elements)
+  if(headersOnly){ list(id=id, head=head) } else {
+    namez <- rbf(todf(x$names))
+    identifiers <- rbf(todf(x$identifiers))
+    status <- x$statuses[[1]]$status
+    platform <- x$platforms[[1]]$platform
+    relocations <- todf(x$relocations)[[1]]
+    remarks <- rbf(todf(x$remarks))
+    updates <- todf(x$updates)[[1]]
+    elements <- rbf(todf(x$elements))
+    location <- parse_loc(x$location)
+    list(id=id, head=head, namez=namez, identifiers=identifiers, 
+         status=status, platform=platform, relocations=relocations, 
+         remarks=remarks, updates=updates, elements=elements, location=location)
+  }
 }
-
-todf <- function(x) ifn_null(x, lapply(x, data.frame, stringsAsFactors = FALSE))
-rbf <- function(x) ifn_null(x, do.call(rbind.fill, x))
-ifn_null <- function(x, y) if(is.null(x)) x else y
-ifn_na <- function(x) if(is.null(x)) NA else x
 
 parse_loc <- function(y){
   id <- y$ncdcstnId
-  descriptions <- unlist(y$descriptions)
-#   todf(y$latitudes)[[1]]
-#   todf(y$longitudes)[[1]]
+  descriptions <- rbf(todf(y$descriptions))
   latlon <- todf(y$latLonPairs)[[1]]
   elev <- todf(y$elevations)[[1]]
   topography <- unlist(y$topography)
   obstructions <- rbf(todf(y$obstructions))
-  geoinfo <- data.frame(ncdstnId=y$geoInfo$ncdcstnId, 
-             country=y$geoInfo$countries[[1]]$country,
-             state=y$geoInfo$stateProvinces[[1]]$stateProvince,
-             county=y$geoInfo$counties[[1]]$county,
-             utcoffset=y$geoInfo$utcOffsets[[1]]$utcOffset, 
+  geoinfo <- data.frame(ncdstnId=ifn_na(y$geoInfo$ncdcstnId), 
+             country=ifn_na(y$geoInfo$countries[[1]]$country),
+             state=ifn_na(y$geoInfo$stateProvinces[[1]]$stateProvince),
+             county=ifn_na(y$geoInfo$counties[[1]]$county),
+             utcoffset=ifn_na(y$geoInfo$utcOffsets[[1]]$utcOffset),
              stringsAsFactors = FALSE)
   nwsinfo <- data.frame(ncdstnId=ifn_na(y$nwsInfo$ncdcstnId),
              climateDivisions=ifn_na(todf(y$nwsInfo$climateDivisions)[[1]]),
@@ -124,5 +124,40 @@ parse_loc <- function(y){
              stringsAsFactors = FALSE)
   list(id=id, description=descriptions, latlon=latlon, elevation=elev,
        topography=topography, obstructions=obstructions, 
-       geoinfo=geoinfo, nwsinfo=nwsinfo)
+       geoinfo=na2null(geoinfo), nwsinfo=na2null(nwsinfo))
 }
+
+combine_stations <- function(w){
+  ids <- vapply(w, "[[", character(1), "id", USE.NAMES = FALSE)
+  head <- rbf(x=lapply(w, "[[", "head"), TRUE)
+  namez <- rbf(lapply(w, "[[", "namez"), TRUE)
+  identifiers <- rbf(lapply(w, "[[", "identifiers"), TRUE)
+  status <- sapply(w, "[[", "status", USE.NAMES = FALSE)
+  platform <- unname(sapply(w, "[[", "platform", USE.NAMES = FALSE))
+  relocations <- rbf(x=lapply(w, "[[", "relocations"), TRUE)
+  remarks <- rbf(x=lapply(w, "[[", "remarks"), TRUE)
+  updates <- rbf(x=lapply(w, "[[", "updates"), TRUE)
+  elements <- rbf(x=lapply(w, "[[", "elements"), TRUE)
+  location <- lapply(w, "[[", "location")
+  list(id=ids, head=head, namez=namez, identifiers=identifiers,
+       status=status, platform=platform, relocations=relocations, 
+       remarks=remarks, updates=updates, elements=elements, location=location)
+}
+
+homr_base <- function() 'http://www.ncdc.noaa.gov/homr/services/station/'
+
+tl <- function(x) if(is.null(x)) NULL else tolower(x)
+todf <- function(x) ifn_null(x, lapply(x, data.frame, stringsAsFactors = FALSE))
+rbf <- function(x, name=FALSE){
+  if(!name){
+    ifn_null(x, do.call(rbind.fill, x))
+  } else {
+    x <- noaa_compact(x)
+    nmz <- names(x)
+    tmp <- Map(function(a, b) data.frame(id=b, a, stringsAsFactors = FALSE), x, nmz)
+    ifn_null(tmp, do.call(rbind.fill, tmp))
+  }
+}
+ifn_null <- function(x, y) if(is.null(x)) x else y
+ifn_na <- function(x) if(is.null(x)) NA else x
+na2null <- function(x) if(all(is.na(x))) NULL else x
---
+++ R/homr_definitions.R
@@ -0,0 +1,22 @@
+#' Historical Observing Metadata Repository (HOMR) station metadata - definitions
+#'
+#' @export
+#'
+#' @param ... Named parameters, such as \code{query}, \code{path}, etc, passed on to 
+#' \code{\link[httr]{modify_url}}. Unnamed parameters will be combined with 
+#' \code{\link[httr]{config}}. 
+#'
+#' @examples \donttest{
+#' head( homr_definitions() )
+#' }
+
+homr_definitions <- function(...){
+  res <- GET(paste0(homr_base(), "search"), query=list(qid='COOP:046742'), ...)
+  out <- content(res, "text")
+  json <- jsonlite::fromJSON(out, FALSE)
+  parse_defs(json$stationCollection$definitions)
+}
+
+parse_defs <- function(x){
+  if(is.null(x)) NULL else do.call(rbind.fill, lapply(x, data.frame, stringsAsFactors = FALSE))
+}
