1aa76de11fb348d55c472131eacca34f5b1d131c
Ed Weber <ed.weber@noaa.gov>
2014-11-07 20:28:39 -0800

    fix trailing newline in read_table from memory

--- R/zzz.r
+++ R/zzz.r
@@ -207,7 +207,8 @@ read_upwell <- function(x){
 
 read_table <- function(x){
   if(is(x, "response")) {
-    read.csv(text = content(x, "text"), sep = ",", stringsAsFactors=FALSE,
+    txt <- gsub('\n$', '', content(x, "text"))
+    read.csv(text = txt, sep = ",", stringsAsFactors=FALSE,
              blank.lines.skip=FALSE)[-1, , drop=FALSE]
   } else {  
     read.delim(x, sep=",", stringsAsFactors=FALSE,
