970524ffdf91d29d832f9d72f264eb79cc055b81
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-17 12:31:52 -0800

    fixed up some ncdc legacy fxns, added get data fxn, fixed param defs  #54

--- R/ncdc_legacy.R
+++ R/ncdc_legacy.R
@@ -1,36 +1,51 @@
 #' Search the NOAA NCDC legacy API - main purpose to get ISD daily data.
 #' @name ncdc_legacy
-#' @param dataset Dataset id, default: isd
-#' @param variable Variable to get.
+#' @param dataset Dataset id, one of isd (default), ish, or daily.
+#' @param variable A variable id.
+#' @param station A station id.
 #' @param token Your token. Store as environment variable as NOAA_LEG_TOKEN or option variable 
 #' as noaalegtoken.
 #' @param state_id State ID
 #' @param state_abbrev State abbreviation
 #' @param country_id Country id
 #' @param country_abbrev Country abbreviation
+#' @param start_date,end_date Date to start and end search for. Valid formats include, YYYY, 
+#' YYYYMM, YYYYMMDD, YYYYMMDDhh, and YYYYMMDDhhmm. 
 #' @param ... Further args passed to \code{\link[httr]{GET}}
 #' @references \url{http://www7.ncdc.noaa.gov/rest/}
+#' @details BEWARE: this service is down a lot. For example, you could make a call, its fine, then 
+#' make a another 5 seconds later, it will be down (giving a 503 HTTP error), then call again and
+#' it's fine. So anyway, take note.
 #' @examples \donttest{
+#' # Variables
+#' ## All variables
 #' ncdc_leg_variables()
-#' ncdc_leg_variables(variable = 'TMP')
-#' ncdc_leg_variables(variable = 474)
-#' head( ncdc_leg_sites() )
+#' ## Specific variables
+#' ### Air Temperature Observation
+#' ncdc_leg_variables(variable = "TMP")
+#' ### Snow Accumulation For The Month
+#' ncdc_leg_variables(variable = "AN1")
 #' 
-#' # Site information
+#' # Sites
+#' head( ncdc_leg_sites() )
 #' ncdc_leg_sites(country_id = 1)
-#' ncdc_leg_sites(country_abbrev = 'US')
+#' ncdc_leg_sites(country_abbrev = 'CA')
 #' ncdc_leg_sites(state_id = 11)
 #' ncdc_leg_sites(state_abbrev = 'HI')
+#' ## site info
+#' ncdc_leg_site_info(stationid=71238099999)
+#' 
+#' # Data
+#' ncdc_leg_data('isd', 71238099999, 'TMP', 200101010000, 200101312359)
+#' ncdc_leg_data('isd', 71238099999, 'TMP', 200101, 200201)
 #' }
 
 #' @export
 #' @rdname ncdc_legacy
 ncdc_leg_variables <- function(dataset = "isd", variable = NULL, token = NULL, ...){
-  base <- paste0(ncdclb(), "variables/")
-  url <- if(is.null(variable)) paste0(base, dataset) else paste0(base, dataset, '/', variable)
+  url <- if(is.null(variable)) paste0(ncdclb(), "variables/", dataset) else paste0(paste0(ncdclb(), "variableinfo/"), dataset, '/', variable)
   args <- list(output = "csv", token = check_token_leg(token))
-  res <- ncdcl_GET(url, args, ...)
-  read.delim(text = res, sep = ",", stringsAsFactors = FALSE, header = FALSE)
+  ncdcl_GET(url, args, ...)
 }
 
 #' @export
@@ -41,13 +56,27 @@ ncdc_leg_sites <- function(dataset = "isd", state_id = NULL, state_abbrev = NULL
   base <- paste0(ncdclb(), "sites/")
   param <- noaa_compact(list(state_id=state_id, state_abbrev=state_abbrev, 
                     country_id=country_id, country_abbrev=country_abbrev))
-  url <- if(length(param) == 0) 
-    paste0(base, dataset) 
-  else 
-    paste0(base, dataset, '/', switch_name(names(param)), '/', param[[1]])
+  url <- if(length(param) == 0) paste0(base, dataset) else paste0(base, dataset, '/', switch_name(names(param)), '/', param[[1]])
   args <- list(output = "csv", token = check_token_leg(token))
-  res <- ncdcl_GET(url, args, ...)
-  read.delim(text = res, sep = ",", stringsAsFactors = FALSE, header = FALSE)
+  ncdcl_GET(url, args, ...)
+}
+
+#' @export
+#' @rdname ncdc_legacy
+ncdc_leg_site_info <- function(dataset = "isd", station, ...)
+{
+  url <- file.path(paste0(ncdclb(), "siteinfo"), dataset, station)
+  args <- list(output = "csv", token = check_token_leg(token))
+  ncdcl_GET(url, args, ...)
+}
+
+#' @export
+#' @rdname ncdc_legacy
+ncdc_leg_data <- function(dataset = "isd", station, variable, start_date, end_date, token = NULL, ...)
+{
+  base <- file.path(ncdclb(), "values", dataset, station, variable, start_date, end_date)
+  args <- list(output = "csv", token = check_token_leg(token))
+  ncdcl_GET(url, args, ...)
 }
 
 switch_name <- function(x){
@@ -55,16 +84,31 @@ switch_name <- function(x){
          country_id='countryId', country_abbrev='countryAbbrev')
 }
 
-ncdclb <- function() 'http://www7.ncdc.noaa.gov/rest/services/'
-
 ncdcl_GET <- function(url, args, ...){
-  res <- GET(url, query = args, ...)
-  stop_for_status(res)
-  stopifnot(res$headers$`content-type` == "text/plain; charset=UTF-8")
-  content(res, "text")
+  res <- GET(url, query = args)
+  if(res$status_code > 200){
+    if(res$status_code == 503) stop(get_retry(res), call. = FALSE) else stop_for_status(res)
+  } else {
+    stopifnot(res$headers$`content-type` == "text/plain; charset=UTF-8")
+    txt <- content(res, "text")
+    read.delim(text = txt, sep = ",", stringsAsFactors = FALSE, header = FALSE)
+  }
 }
 
 check_token_leg <- function(x){
   tmp <- if(is.null(x)) Sys.getenv("NOAA_LEG_TOKEN", "") else x
   if(tmp == "") getOption("noaalegtoken", stop("need an API token for NOAA legacy API")) else tmp
 }
+
+get_retry <- function(x){
+  sprintf("503: Retry after %s seconds", as.numeric(x$headers$`retry-after`))
+}
+
+get_error <- function(x){
+  html <- content(x)
+  title <- xpathSApply(html, "//title", xmlValue)
+  desc <- paste0(xpathApply(html, "//u", xmlValue), collapse = "")
+  paste0(" - ", title, desc, collapse = "")
+}
+
+ncdclb <- function() 'http://www7.ncdc.noaa.gov/rest/services/'
