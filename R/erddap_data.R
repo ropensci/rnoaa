fb9fa26db3b74715a4cc33fbb5b375e7be3b39dc
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-19 08:55:39 -0800

    added defunct man file back for erddap_data, closes #65

---
+++ R/erddap_data.R
@@ -0,0 +1,9 @@
+#' Get ERDDAP data.
+#'
+#' @export
+#' @name erddap_data-defunct
+
+erddap_data <- function()
+{
+  .Defunct(msg = "See functions erddap_grid and erddap_table for griddap and tabledap data, respectively")
+}
