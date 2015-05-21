9f23a1d75bef3b29fc16663b319a9917002919b4
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-21 16:08:18 -0700

    note about reprojecting fix #23

--- R/seaice.r
+++ R/seaice.r
@@ -5,6 +5,8 @@
 #' @param ... Further arguments passed on to readshpfile function, see
 #'    \code{readshpfile}
 #' @return A data.frame
+#' @details If you want to reproject the shape files, use 
+#' \code{\link{readshpfile}} to read in shape file, then reproject, and so on.
 #' @export
 #' @examples \dontrun{
 #' # Look at data.frame's for a series of years for Feb, South pole
@@ -33,8 +35,7 @@
 #'   theme_ice() +
 #'   facet_wrap(~ .id)
 #' }
-seaice <- function(url, ...)
-{
+seaice <- function(url, ...) {
   tt <- readshpfile(url, ...)
   fortify(tt)
 }
@@ -61,41 +62,46 @@ seaice <- function(url, ...)
 #' }
 #' @export
 #' @keywords internal
-seaiceeurls <- function(yr=NULL, mo=NULL, pole=NULL)
-{
-  eachmonth <- sprintf('ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/shapefiles/%s/shp_extent/', month.abb)
-  yrs_prev <- seq(1979, year(today())-1, 1)
-  months_prevyr <- c(paste0(0, seq(1,9)), c(10,11,12))
-  yrs_months <- do.call(c, lapply(yrs_prev, function(x) paste(x, months_prevyr, sep='')))
-  urls <- do.call(c, lapply(c('S','N'), function(x) paste(eachmonth, 'extent_', x, '_', yrs_months, '_polygon.zip', sep='')))
+seaiceeurls <- function(yr=NULL, mo=NULL, pole=NULL) {
+  eachmonth <- sprintf(seaiceftp(), month.abb)
+  yrs_prev <- seq(1979, year(today()) - 1, 1)
+  months_prevyr <- c(paste0(0, seq(1, 9)), c(10, 11, 12))
+  yrs_months <- do.call(c, lapply(yrs_prev, function(x) paste(x, months_prevyr, sep = '')))
+  urls <- do.call(c, lapply(c('S', 'N'), function(x) {
+    paste(eachmonth, 'extent_', x, '_', yrs_months, '_polygon.zip', sep = '')
+  }))
 
   months_thisyr <- paste0(0, seq(1,month(today())))
   yrs_months_thisyr <- paste0(2013, months_thisyr)
-  eachmonth_thiyr <- eachmonth[1:grep(month(today() - months(1), label=TRUE, abbr=TRUE), eachmonth)]
-  urls_thisyr <- do.call(c, lapply(c('S','N'), function(x) paste(eachmonth_thiyr, 'extent_', x, '_', yrs_months_thisyr, '_polygon.zip', sep='')))
+  eachmonth_thiyr <- eachmonth[1:grep(month(today() - months(1), label = TRUE, abbr = TRUE), eachmonth)]
+  urls_thisyr <- do.call(c, lapply(c('S', 'N'), function(x) {
+    paste(eachmonth_thiyr, 'extent_', x, '_', yrs_months_thisyr, '_polygon.zip', sep = '')
+  }))
 
   allurls <- c(urls, urls_thisyr)
 
-  if(!is.null(pole)) pole <- sprintf("_%s_", pole)
-  if(!is.null(yr)) yr <- sprintf("_%s", yr)
+  if (!is.null(pole)) pole <- sprintf("_%s_", pole)
+  if (!is.null(yr)) yr <- sprintf("_%s", yr)
 
-  if(!is.null(yr) & is.null(mo) & is.null(pole))
-    ss <- grep(yr, allurls, value=TRUE)
-  if(is.null(yr) & !is.null(mo) & is.null(pole))
-    ss <- grep(mo, allurls, value=TRUE)
-  if(is.null(yr) & is.null(mo) & !is.null(pole))
-    ss <- grep(pole, allurls, value=TRUE)
-  if(!is.null(yr) & !is.null(mo) & is.null(pole))
-    ss <- grep(yr, grep(mo, allurls, value=TRUE), value=TRUE)
-  if(!is.null(yr) & is.null(mo) & !is.null(pole))
-    ss <- grep(yr, grep(pole, allurls, value=TRUE), value=TRUE)
-  if(is.null(yr) & !is.null(mo) & !is.null(pole))
-    ss <- grep(pole, grep(mo, allurls, value=TRUE), value=TRUE)
-  if(!is.null(yr) & !is.null(mo) & !is.null(pole))
-    ss <- grep(yr, grep(pole, grep(mo, allurls, value=TRUE), value=TRUE), value=TRUE)
+  if (!is.null(yr) & is.null(mo) & is.null(pole))
+    ss <- grep(yr, allurls, value = TRUE)
+  if (is.null(yr) & !is.null(mo) & is.null(pole))
+    ss <- grep(mo, allurls, value = TRUE)
+  if (is.null(yr) & is.null(mo) & !is.null(pole))
+    ss <- grep(pole, allurls, value = TRUE)
+  if (!is.null(yr) & !is.null(mo) & is.null(pole))
+    ss <- grep(yr, grep(mo, allurls, value = TRUE), value = TRUE)
+  if (!is.null(yr) & is.null(mo) & !is.null(pole))
+    ss <- grep(yr, grep(pole, allurls, value = TRUE), value = TRUE)
+  if (is.null(yr) & !is.null(mo) & !is.null(pole))
+    ss <- grep(pole, grep(mo, allurls, value = TRUE), value = TRUE)
+  if (!is.null(yr) & !is.null(mo) & !is.null(pole))
+    ss <- grep(yr, grep(pole, grep(mo, allurls, value = TRUE), value = TRUE), value = TRUE)
 
   return( ss )
 }
+
+seaiceftp <- function() 'ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/shapefiles/%s/shp_extent/'
 
 #' Function to read shapefiles
 #' @param x A url
@@ -103,31 +109,34 @@ seaiceeurls <- function(yr=NULL, mo=NULL, pole=NULL)
 #' @return An object of class sp
 #' @export
 #' @keywords internal
-readshpfile <- function(x, storepath=NULL) {
+readshpfile <- function(x, storepath = NULL) {
   filename <- strsplit(x, '/')[[1]][length(strsplit(x, '/')[[1]])]
   filename_noending <- strsplit(filename, "\\.")[[1]][[1]]
-  if(is.null(storepath)){ storepath <- tempdir() }
+  if (is.null(storepath)) {
+    storepath <- tempdir()
+  }
   path_write <- paste0(storepath, '/', filename_noending)
   path <- paste0(storepath, '/', filename)
-  bb <- try(download.file(x, path), silent=TRUE)
-  if(class(bb) == "try-error")
+  bb <- try(download.file(x, path), silent = TRUE)
+  if (class(bb) == "try-error") {
     stop('Data not available, ftp server may be down')
-  dir.create(path_write, showWarnings=FALSE)
-  unzip(path, exdir=path_write)
+  }
+  dir.create(path_write, showWarnings = FALSE)
+  unzip(path, exdir = path_write)
   my_layer <- ogrListLayers(path.expand(path_write))
-  readOGR(path.expand(path_write), layer=my_layer)
+  readOGR(path.expand(path_write), layer = my_layer)
 }
 
 #' ggplot2 map theme
 #' @import ggplot2
 #' @export
 #' @keywords internal
-theme_ice <- function(){
-  list(theme_bw(base_size=18),
+theme_ice <- function() {
+  list(theme_bw(base_size = 18),
        theme(panel.border = element_blank(),
              panel.grid.major = element_blank(),
              panel.grid.minor = element_blank(),
              axis.text = element_blank(),
              axis.ticks = element_blank()),
-       labs(x='',y=''))
+       labs(x = '', y = ''))
 }
