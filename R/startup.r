84d2da53ccc38616d40b23c85e327a08a224fcbe
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-21 12:27:37 -0700

    removed startup message, bumped patch version

--- R/startup.r
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-	packageStartupMessage("\n\n New to rnoaa? See package vignettes,\n and see tutorial at http://ropensci.org/tutorials/rnoaa_tutorial.html. \n Use suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-} 
