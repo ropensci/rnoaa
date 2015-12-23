9673f95bd3f9a49c282e5a9a9ca93121a1d94470
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-23 12:30:15 -0800

    finished off argo, for now at least, fix #123

--- R/argo.R
+++ R/argo.R
@@ -2,50 +2,87 @@
 #'
 #' @export
 #' @name argo
-#' @param gt gt string
-#' @param of of string
-#' @param qwmo qwmo string
-#' @param wmo wmo string
-#' @param box Bounding box, of the form: min lon, min lat, max lon, max lat
-#' @param limit (integer) number to return
-#' @param ... Curl options passed on to \code{\link[httr]{GET}}. Optional
-#' @references \url{http://www.ifremer.fr/lpo/naarc/m/docs/api/howto.html}
-#' @examples \dontrun{
-#' argo_search("n", limit = 3)
-#' argo_search("np", limit = 3)
-#' argo_search("nf", limit = 3)
-#' argo_search("coord", limit = 3)
-#' argo_search("fullcoord", limit = 5)
-#' argo_search("list", "dac", limit = 5)
-#' argo_search(qwmo = 49, limit = 3)
-#' argo_search("n", box = c(-40, 35, 3, 2))
-#' 
-#' argo_files(wmo = 4900881)
-#' }
-argo_search <- function(gt = NULL, of = NULL, qwmo = NULL, 
-                 wmo = NULL, box=NULL, limit = 10, ...) {
-  args <- noaa_compact(list(get = gt, of = of, qwmo = qwmo, 
-                            wmo = wmo, file = file, box = box, 
-                            limit = limit))
-  argo_GET(argo_base(), args, ...)
+#' @template argo_params
+#' @template argo_egs
+argo_search <- function(func = NULL, of = NULL, qwmo = NULL, wmo = NULL, box=NULL, 
+                        area = NULL, around = NULL, year = NULL, yearmin = NULL, 
+                        yearmax = NULL, month = NULL, monthmin = NULL, monthmax = NULL, 
+                        lr = NULL, from =NULL, to = NULL, dmode = NULL, 
+                        pres_qc = NULL, temp_qc = NULL, psal_qc = NULL, doxy_qc = NULL, 
+                        ticket = NULL, limit = 10, ...) {
+  
+  args <- noaa_compact(list(get = func, of = of, qwmo = qwmo, wmo = wmo, file = file, 
+      box = box, area = area, around = around, year = year, yearmin = yearmin, 
+      yearmax = yearmax, month = month, monthmin = monthmin, monthmax = monthmax,
+      lr = lr, from = from, to = to, dmode = dmode, pres_qc = pres_qc, temp_qc = temp_qc, 
+      psal_qc = psal_qc, doxy_qc = doxy_qc, ticket = ticket, limit = limit))
+  res <- argo_GET(argo_api(), args, ...)
+  jsonlite::fromJSON(content(res, "text"))
 }
 
 #' @export
 #' @rdname argo
-argo_files <- function(gt = NULL, of = NULL, qwmo = NULL, 
-                       wmo = NULL, box=NULL, ...) {
-  args <- noaa_compact(list(get = gt, of = of, qwmo = qwmo, 
-                            wmo = wmo, file = "", box = box))
-  argo_GET(argo_base(), args, ...)
+argo_files <- function(wmo = NULL, cyc = NULL, ...) {
+  args <- noaa_compact(list(wmo = wmo, cyc = cyc, file = ""))
+  res <- argo_GET(argo_api(), args, ...)
+  jsonlite::fromJSON(content(res, "text"))
+}
+
+#' @export
+#' @rdname argo
+argo_qwmo <- function(qwmo, limit = 10, ...) {
+  args <- noaa_compact(list(qwmo = qwmo, limit = limit))
+  res <- argo_GET(argo_api(), args, ...)
+  jsonlite::fromJSON(content(res, "text"))
+}
+
+#' @export
+#' @rdname argo
+argo_plan <- function(...) {
+  args <- noaa_compact(list(plan = ""))
+  res <- argo_GET(argo_api(), args, ...)
+  jsonlite::fromJSON(content(res, "text"))
+}
+
+#' @export
+#' @rdname argo
+argo_buoy_files <- function(dac, id, ...) {
+  tfile <- tempfile(fileext = ".txt")
+  url <- paste0(argo_ftp(), sprintf('dac/%s/%s/profiles/', dac, id))
+  download.file(url, destfile = tfile, quiet = TRUE)
+  res <- readLines(tfile, warn = FALSE)
+  tab <- read.table(text = res, stringsAsFactors = FALSE)
+  tab[,NCOL(tab)]
+}
+
+#' @export
+#' @rdname argo
+argo <- function(dac, id, cycle, dtype, path = "~/.rnoaa/argo", overwrite = TRUE, ...) {
+  path <- file.path(path, dac)
+  apath <- a_local(dac, id, cycle, dtype, path)
+  if (!is_isd(apath)) {
+    dir.create(path, showWarnings = FALSE, recursive = TRUE)
+    suppressWarnings(argo_GET(a_remote(dac, id, cycle, dtype), list(), write_disk(apath, overwrite), ...))
+  }
+  message(sprintf("<path> %s", apath), "\n")
+  ncdf4::nc_open(apath)
 }
 
 # helpers -----------------------------------
-argo_GET <- function(url, args, ...) {
+argo_GET <- function(url, args = list(), ...) {
   res <- GET(url, query = args, ...)
   stop_for_status(res)
-  jsonlite::fromJSON(content(res, "text"))  
+  res
 }
 
-argo_base <- function() "http://www.ifremer.fr/lpo/naarc/api/v1"
+argo_base <- function() "http://www.ifremer.fr/"
+argo_api <- function() paste0(argo_base(), "lpo/naarc/api/v1")
+argo_ftp <- function() "ftp://ftp.ifremer.fr/ifremer/argo/"
 
-# http://www.ifremer.fr/lpo/naarc/api/v1/?file&wmo=4900881
+a_local <- function(dac, id, cycle, dtype, path) {
+  file.path(path, sprintf("%s%s_%s.nc", dtype, id, cycle))
+}
+
+a_remote <- function(dac, id, cycle, dtype) {
+  file.path(argo_ftp(), "dac", dac, id, "profiles", sprintf("%s%s_%s.nc", dtype, id, cycle))
+}
