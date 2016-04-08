19a6e059ae3b87fe5828704e718d9c111071882e
Brooke Anderson <geanderson@jhsph.edu>
2016-04-08 17:20:51 -0600

    Work on documentation and small fixes for `meteo_distance` functions

--- R/meteo_distance.R
+++ R/meteo_distance.R
@@ -1,6 +1,6 @@
 #' meteo_distance
 #'
-#' @param data a dataframe. Expects col headers with names latName and longName
+#' @param station_data a dataframe. Expects col headers with names latName and longName
 #' @param lat Latitude to centre search at
 #' @param long Longitude to centre search at
 #' @param latName Name of latitude header name in data, Default = 'latitude'
@@ -12,17 +12,13 @@
 #'
 #' @return a dataframe in a column with the distance of stations
 #' @export
-meteo_distance <- function(data,
-                           lat,
-                           long,
+meteo_distance <- function(station_data, lat, long,
                            latName = 'latitude',
                            longName = 'longitude',
-                           units = 'deg',
-                           radius=NULL,
-                           limit = NULL) {
+                           units = 'deg', radius = NULL, limit = NULL) {
 
   data <- meteo_process_geographic_data(
-    data = data,
+    data = station_data,
     lat = lat,
     long = long,
     latName = latName,
@@ -42,12 +38,7 @@ meteo_distance <- function(data,
 
 #' meteo_process_geographic_data
 #'
-#' @param data data
-#' @param lat data
-#' @param long data
-#' @param latName data
-#' @param longName data
-#' @param units data
+#' @inheritParams meteo_distance
 #'
 #' @export
 meteo_process_geographic_data <- function(data,
@@ -62,7 +53,9 @@ meteo_process_geographic_data <- function(data,
 
   # Check if lat, long exists as headers in the data frame
   if (!all(c(latName, longName) %in% colnames(data))) {
-    stop('Error, missing header label. Expected latName and longName')
+    stop(paste('Error, missing header label. Expected columns in the station',
+               'dataset with column names as specified by the latName and',
+               'longName arguments.'))
   } # End check for header ontology
 
   # Add new column to store distance from given location ([lat, lon] point)
@@ -73,8 +66,8 @@ meteo_process_geographic_data <- function(data,
     meteo_spherical_distance(
       lat1 = lat,
       long1 = long,
-      lat2 = data[latName][1:nrow(data),],
-      long2 = data[longName][1:nrow(data),],
+      lat2 = data[ , latName],
+      long2 = data[ , longName],
       units = 'deg'
     )
 
@@ -84,20 +77,22 @@ meteo_process_geographic_data <- function(data,
   return(data)
 } # End meteo_process_geographic_data
 
-#' meteo_spherical_distance
+#' Calculate the distance between two locations
+#'
+#' This functions uses the haversine formula to calculate the great circle
+#' distance between two locations, identified by their latitudes and longitudes.
 #'
 #' @param lat1 data
 #' @param long1 data
 #' @param lat2 data
 #' @param long2 data
-#' @param units data
+#' @inheritParams meteo_distance
+#'
+#' @return A numeric vector giving the distance (in kilometers) between the
+#'    pair of locations input.
 #'
 #' @export
-meteo_spherical_distance <- function(lat1,
-                                     long1,
-                                     lat2,
-                                     long2,
-                                     units = 'deg') {
+meteo_spherical_distance <- function(lat1, long1, lat2, long2, units = 'deg') {
 
   radius_earth <- 6371
 
@@ -109,6 +104,8 @@ meteo_spherical_distance <- function(lat1,
     long2 <- deg2rad(long2)
   } else if (units == 'dms') {
     stop("dms to rad function currently under dev")
+  } else if(units != 'rad'){
+    stop("The `units` argument must be one of: `deg`, `rad`, or `dms`.")
   }
 
   # Determine distance using the haversine formula, assuming a spherical earth
@@ -121,51 +118,75 @@ meteo_spherical_distance <- function(lat1,
 
 } # End calculate_spherical_distance
 
-#' deg2rad
+#' Convert from degrees to radians
 #'
-#' @param deg
+#' @param deg A numeric vector in units of degrees.
+#'
+#' @return The input numeric vector, converted to units of radians.
 #'
 #' @export
 deg2rad <- function(deg) {
   return(deg*pi/180)
 } # End deg2rad
 
-#' get_nearby_stations
+#' Find weather monitors near locations
 #'
-#' @description
+#' This function inputs a dataframe with latitudes and longitudes of locations
+#' and creates a dataframe with monitors with a certain radius of those
+#' locations. The site ids from this dataframe can then be used with other
+#' \code{rnoaa} functions to pull data from all available weather stations near
+#' a location.
 #'
 #' @param lat_lon_df A dataframe that contains the site latitude and longitude
 #'    values used to search for nearby weather stations
-#' @param lat_colname Name of the latitude column in the lat_lon_df
-#' @param lon_colname Name of the longitude column in the lat_lon_df
-#' @param ghcnd_station_list List of weather stations obtained using
-#'    `ghcnd_stations()`
-#' @param radius Radius to search (in km)
+#' @param lat_colname A character string giving the name of the latitude column
+#'    in the \code{lat_lon_df} dataframe.
+#' @param lon_colname A character string giving the name of the longitude column
+#'    in the \code{lat_lon_df} dataframe.
+#' @param station_data The output of \code{\link{ghcnd_stations()[[1]]}}:
+#'    a current list of weather stations available through NOAA for the GHCND
+#'    dataset. The format of this is a list with a single element, a dataframe
+#'    with one row per available weather station.
+#' @param radius A numeric vector giving the radius (in kilometers) within which
+#'    to search for monitors near a location
 #'
 #' @return a dataframe containing a unique set of the weather stations within
-#'    the search radius
+#'    the search radius. Site IDs for the weather stations given in this
+#'    dataframe can be used in conjunction with other functions in the
+#'    \code{rnoaa} package to pull weather data for the station.
+#'
+#' @note By default, this function will pull the full station list from NOAA
+#'    to use to identify nearby locations. If you will be creating lists of
+#'    monitors nearby several stations, you can save some time by using the
+#'    \code{\link{ghcnd_stations}} function separately to create an object
+#'    with all stations and then use the argument \code{ghcnd_station_list} in
+#'    this function to reference that object, rather than using this function's
+#'    defaults.
 #'
 #' @examples
+#' \dontrun{
 #' lat <- c(37.779199, 37.531635)
 #' lon <- c(-122.404294, -122.419282)
 #' lat_lon_df <- data.frame(latitude = lat, longitude = lon)
 #'
+#' stations <- ghncd_stations()
 #' nearby_stations <-
-#' get_nearby_stations(lat_lon_df = test_df, lat_colname = 'latitude',
-#' lon_colname = 'longitude', ghcnd_station_list = stations, radius = 20)
+#' meteo_nearby_stations(lat_lon_df, lat_colname = 'latitude',
+#'    lon_colname = 'longitude', ghcnd_station_list = stations, radius = 20)
+#' }
 #'
 #' @export
 meteo_nearby_stations <- function(lat_lon_df, lat_colname = "lat",
                                   lon_colname = "lon",
-                                  ghcnd_station_list = ghcnd_stations(),
-                                  radius, ...)
+                                  station_data = ghcnd_stations()[[1]],
+                                  radius = 25, ...)
   lat_lon_df %>%
   dplyr::distinct_(lat_colname, lon_colname) %>%
   split(.[, lat_colname], .[, lon_colname]) %>%
   purrr::map(function(x) {
     station_ids <-
-      meteo_distance(data = ghcnd_station_list, lat = x$latitude,
-                     long = x$longitude, radius = radius) %>%
+      meteo_distance(station_data = station_data, lat = x[lat_colname],
+                     long = x[lon_colname], radius = radius) %>%
       distinct(id)
     station_ids <- dplyr::rbind_all(station_ids)
     return(station_ids)
