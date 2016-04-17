733fdb9942653f2c76cb1a1646e2fd9465e8e587
Brooke Anderson <geanderson@jhsph.edu>
2016-04-17 12:44:54 -0600

    Clean up `helpers_ghncd.R`

--- R/helpers_ghcnd.R
+++ R/helpers_ghcnd.R
@@ -2,19 +2,22 @@
 #'
 #' This function takes a vector of one or more weather station IDs. It will pull
 #' the weather data from the Global Historical Climatology Network's daily
-#' data for each of the stations and join them together in a single tidy
+#' data (GHCND) for each of the stations and join them together in a single tidy
 #' dataframe. For any weather stations that the user calls that are not
-#' available by ftp from GHCN, the function will return a warning
+#' available by ftp from GHCND, the function will return a warning
 #' giving the station ID.
 #'
 #' @param monitors A character vector listing the station IDs for all
-#'    weather stations the user would like to pull.
+#'    weather stations the user would like to pull. To get a full and
+#'    current list of stations, the user can use the \code{\link{ghcnd_stations}}
+#'    function. To identify stations within a certain radius of a location, the
+#'    user can use the \code{\link{meteo_nearby_stations}} function.
 #' @inheritParams meteo_tidy_ghcnd
+#' @inheritParams ghcnd_search
 #'
-#' @return A data frame of daily weather data for a multiple weather monitors,
+#' @return A data frame of daily weather data for multiple weather monitors,
 #'    converted to a tidy format. All weather variables may not exist for all
-#'    weather stations, but the returned dataset would have at most the
-#'    following columns:
+#'    weather stations. Examples of variables returned are:
 #'    \itemize{
 #'    \item \code{id}: Character string with the weather station site id
 #'    \item \code{date}: Date of the observation
@@ -26,14 +29,42 @@
 #'    \item \code{wsfg}: Peak gust wind speed, in meters / second
 #'    }
 #'    There are other possible weather variables in the Global Historical
-#'    Climatology Network, but we have not implemented cleaning them through
-#'    this function. See
+#'    Climatology Network; see
 #'    \url{http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt} for a full
-#'    list. If you are interested in having other weather variables from this
-#'    list added to this function, you can submit a request to this package's
-#'    GitHub issues page and we may be able to add it.
+#'    list. If the \code{var} argument is something other than "all", then
+#'    only variables included in that argument will be included in the output
+#'    data frame. The variables \code{prcp}, \code{tmax}, \code{tmin}, and \code{tavg}
+#'    have all been converted from tenths of their metric to the metric (e.g.,
+#'    from tenths of degrees Celsius to degrees Celsius). All other variables
+#'    are in the units specified in the linked file.
+#'
+#' @note The weather flags, which are kept by specifying
+#' \code{keep_flags = TRUE} are:
+#' \itemize{
+#' \item \code{*_mflag}: Measurement flag, which gives some information on how
+#'    the observation was measured.
+#' \item \code{*_qflag}: Quality flag, which gives quality information on the
+#'    measurement, like if it failed to pass certain quality checks.
+#' \item \code{*_sflag}: Source flag. This gives some information on the
+#'    weather collection system (e.g., U.S. Cooperative Summary of the Day,
+#'    Australian Bureau of Meteorology) the weather observation comes from.
+#' }
+#' More information on the interpretation of these flags can be found in the
+#' README file for the NCDC's Daily Global Historical Climatology Network's
+#' data at \url{http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt}.
 #'
 #' @note This function may take a while to run.
+#'
+#' @author Brooke Anderson \email{brooke.anderson@@colostate.edu}
+#'
+#' @references
+#'
+#' For more information about the data pulled with this function, see:
+#'
+#' Menne, M.J., I. Durre, R.S. Vose, B.E. Gleason, and T.G. Houston, 2012:
+#' An overview of the Global Historical Climatology Network-Daily Database.
+#' Journal of Atmospheric and Oceanic Technology, 29, 897-910,
+#' doi:10.1175/JTECH-D-11-00103.1.
 #'
 #' @examples
 #' \dontrun{
@@ -47,8 +78,6 @@
 meteo_pull_monitors <- function(monitors, keep_flags = FALSE, date_min = NULL,
                                 date_max = NULL, var = "all"){
   monitors <- unique(monitors)
-  #safe_ghcnd <- purrr::safely(ghcnd)
-  #all_monitors_ghcnd <- lapply(monitors, safe_ghcnd)
 
   safe_meteo_tidy_ghcnd <- purrr::safely(meteo_tidy_ghcnd)
   all_monitors_clean <- lapply(monitors, safe_meteo_tidy_ghcnd,
@@ -60,7 +89,7 @@ meteo_pull_monitors <- function(monitors, keep_flags = FALSE, date_min = NULL,
   if(length(bad_stations) > 0){
     warning(paste("The following stations could not be pulled from",
                   "the GHCN ftp:\n", paste(bad_stations, collapse = ", "),
-                  "\nAll other monitors were successfully pulled from GHCN."))
+                  "\nAny other monitors were successfully pulled from GHCN."))
   }
 
   all_monitors_out <- lapply(all_monitors_clean[!check_station],
@@ -71,23 +100,17 @@ meteo_pull_monitors <- function(monitors, keep_flags = FALSE, date_min = NULL,
 
 #' Create a tidy GHCND dataset from a single monitor
 #'
-#' This function inputs an object created by \code{\link{ghcnd}} and cleans up the
-#' data into a tidy form.
-#'
-#' The following reference has more information on this data:
-#'
-#' Menne, M.J., I. Durre, R.S. Vose, B.E. Gleason, and T.G. Houston, 2012:
-#' An overview of the Global Historical Climatology Network-Daily Database.
-#' Journal of Atmospheric and Oceanic Technology, 29, 897-910,
-#' doi:10.1175/JTECH-D-11-00103.1.
+#' This function inputs an object created by \code{\link{ghcnd}} and cleans up
+#' the data into a tidy form.
 #'
 #' @param keep_flags TRUE / FALSE for whether the user would like to keep all the flags
-#'    for each weather varialbe. The default is to not keep the flags (FALSE).
+#'    for each weather variable. The default is to not keep the flags (FALSE).
+#'    See the note below for more information on these flags.
 #' @inheritParams ghcnd_search
 #'
 #' @return A data frame of daily weather data for a single weather monitor,
 #'    converted to a tidy format. All weather variables may not exist for all
-#'    weather stations Examples of variables returned are:
+#'    weather stations. Examples of variables returned are:
 #'    \itemize{
 #'    \item \code{id}: Character string with the weather station site id
 #'    \item \code{date}: Date of the observation
@@ -121,6 +144,10 @@ meteo_pull_monitors <- function(monitors, keep_flags = FALSE, date_min = NULL,
 #' README file for the NCDC's Daily Global Historical Climatology Network's
 #' data at \url{http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt}.
 #'
+#' @author Brooke Anderson \email{brooke.anderson@@colostate.edu}
+#'
+#' @seealso \code{\link{meteo_pull_monitors}}
+#'
 #' @examples
 #' \dontrun{
 #' # One station in Australia is ASM00094275
@@ -142,7 +169,7 @@ meteo_tidy_ghcnd <- function(stationid, keep_flags = FALSE, var = "all",
 
   which_vars_tenths <- which(colnames(cleaned_df) %in%
                                 c("prcp", "tmax", "tmin", "tavg"))
-  cleaned_df <- tbl_df(cleaned_df)
+  cleaned_df <- dplyr::tbl_df(cleaned_df)
   # All these variables are in tenths of units
   cleaned_df[, which_vars_tenths] <- vapply(cleaned_df[, which_vars_tenths],
                                              FUN.VALUE = numeric(nrow(cleaned_df)),
@@ -178,6 +205,8 @@ meteo_tidy_ghcnd <- function(stationid, keep_flags = FALSE, var = "all",
 #'
 #' @return A dataframe reformatted to allow easy aggregation of all weather
 #'    variables for a single monitor.
+#'
+#' @author Brooke Anderson \email{brooke.anderson@@colostate.edu}
 meteo_tidy_ghncd_element <- function(x, keep_flags = FALSE){
   var_name <- colnames(x)[2]
   if(keep_flags){
