b1ed31c581d0ad2f53b72c6d09fb442a4e9b6d10
Scott Chamberlain <myrmecocystus@gmail.com>
2016-08-23 12:26:23 -0700

    fixed seaice url construction, fix #160 fixed ncdc_datasets test expectations for dimensions bumped dev version

--- R/seaice.r
+++ R/seaice.r
@@ -54,14 +54,14 @@ seaiceeurls <- function(yr=NULL, mo=NULL, pole=NULL) {
   months_prevyr <- c(paste0(0, seq(1, 9)), c(10, 11, 12))
   yrs_months <- do.call(c, lapply(yrs_prev, function(x) paste(x, months_prevyr, sep = '')))
   urls <- do.call(c, lapply(c('S', 'N'), function(x) {
-    paste(eachmonth, 'extent_', x, '_', yrs_months, '_polygon.zip', sep = '')
+    paste(eachmonth, 'extent_', x, '_', yrs_months, '_polygon_v2.zip', sep = '')
   }))
 
   months_thisyr <- paste0(0, seq(1,month(today())))
   yrs_months_thisyr <- paste0(2013, months_thisyr)
   eachmonth_thiyr <- eachmonth[1:grep(month(today() - months(1), label = TRUE, abbr = TRUE), eachmonth)]
   urls_thisyr <- do.call(c, lapply(c('S', 'N'), function(x) {
-    paste(eachmonth_thiyr, 'extent_', x, '_', yrs_months_thisyr, '_polygon.zip', sep = '')
+    paste(eachmonth_thiyr, 'extent_', x, '_', yrs_months_thisyr, '_polygon_v2.zip', sep = '')
   }))
 
   allurls <- c(urls, urls_thisyr)
--- tests/testthat/test-ncdc_datasets.r
+++ tests/testthat/test-ncdc_datasets.r
@@ -2,12 +2,12 @@ context("ncdc_datasets")
 
 test_that("ncdc_datasets returns the correct class", {
   skip_on_cran()
-  
+
   Sys.sleep(time = 0.5)
   tt <- ncdc_datasets()
   Sys.sleep(time = 0.5)
   uu <- ncdc_datasets(datasetid='ANNUAL')
-  
+
   # class
   expect_is(tt, "ncdc_datasets")
   expect_is(tt$data, "data.frame")
@@ -17,8 +17,8 @@ test_that("ncdc_datasets returns the correct class", {
   expect_is(uu$meta, "NULL")
 
   # dimensions
-  expect_equal(dim(tt$data), c(11,6))
-  expect_equal(length(tt$data$id), 11)
+  expect_equal(dim(tt$data), c(13,6))
+  expect_equal(length(tt$data$id), 13)
   expect_equal(length(uu), 2)
   expect_equal(dim(uu$data), c(1,5))
 })
