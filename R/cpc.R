5dc585a62eb569403a4106271dd23548a5b59341
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-15 14:37:18 -0800

    cpc fxn added fix #193, bump dev version

--- R/cpc.R
+++ R/cpc.R
@@ -2,20 +2,25 @@
 #'
 #' @export
 #' @param date (date/character) date in YYYY-MM-DD format
-#' @param us (logical) us data only? default: \code{FALSE}
+#' @param us (logical) US data only? default: \code{FALSE}
 #' @param ... curl options passed on to \code{\link[crul]{HttpClient}}
 #' @return a data.frame, with columns:
 #' \itemize{
-#'  \item lon - longitude
-#'  \item lat - latitude
+#'  \item lon - longitude (0 to 360)
+#'  \item lat - latitude (-90 to 90)
 #'  \item precip - precipitation (in mm)
 #' }
-#' note that longitude is in 0 to 360 range
+#'
 #' @references \url{http://www.cpc.ncep.noaa.gov/}
 #' ftp://ftp.cpc.ncep.noaa.gov/precip/CPC_UNI_PRCP
+#' ftp://ftp.cpc.ncep.noaa.gov/precip/CPC_UNI_PRCP/GAUGE_CONUS/DOCU/PRCP_CU_GAUGE_V1.0CONUS_0.25deg.README
+#' ftp://ftp.cpc.ncep.noaa.gov/precip/CPC_UNI_PRCP/GAUGE_GLB/DOCU/PRCP_CU_GAUGE_V1.0GLB_0.50deg_README.txt
+#' https://www.esrl.noaa.gov/psd/data/gridded/data.unified.daily.conus.html
+#'
 #' @details
-#' Rainfall data for the world (1979-present), at resolution 50 km, and
+#' Rainfall data for the world (1979-present, resolution 50 km), and
 #' the US (1948-present, resolution 25 km).
+#'
 #' @examples
 #' cpc_prcp(date = "2017-01-15")
 #' cpc_prcp(date = "2015-06-05")
@@ -24,20 +29,22 @@
 #' cpc_prcp(date = "2005-07-09")
 #'
 #' cpc_prcp(date = "2005-07-09", us = TRUE)
-cpc_prcp <- function(date, ...) {
+#' cpc_prcp(date = "2009-08-03", us = TRUE)
+cpc_prcp <- function(date, us = FALSE, ...) {
   assert(date, c("character", "Date"))
   dates <- str_extract_all_(date, "[0-9]+")[[1]]
   assert_range(dates[1], 1979:format(Sys.Date(), "%Y"))
   assert_range(as.numeric(dates[2]), 1:12)
   assert_range(as.numeric(dates[3]), 1:31)
 
-  path <- cpc_get(year = dates[1], month = dates[2], day = dates[3], ...)
-  cpc_read(path)
+  path <- cpc_get(year = dates[1], month = dates[2], day = dates[3],
+                  us = us, ...)
+  cpc_read(path, us)
 }
 
-cpc_get <- function(year, month, day, cache = TRUE, overwrite = FALSE, ...) {
+cpc_get <- function(year, month, day, us, cache = TRUE, overwrite = FALSE, ...) {
   cpc_cache$mkdir()
-  key <- cpc_key(year, month, day)
+  key <- cpc_key(year, month, day, us)
   file <- file.path(cpc_cache$cache_path_get(), basename(key))
   if (!file.exists(file)) {
     suppressMessages(cpc_GET_write(sub("/$", "", key), file, overwrite, ...))
@@ -63,27 +70,46 @@ cpc_GET_write <- function(url, path, overwrite = TRUE, ...) {
   return(res)
 }
 
-cpc_base_ftp <- function() "ftp://ftp.cpc.ncep.noaa.gov/precip/CPC_UNI_PRCP/GAUGE_GLB"
+cpc_base_ftp <- function(x) {
+  base <- "ftp://ftp.cpc.ncep.noaa.gov/precip/CPC_UNI_PRCP"
+  if (x) file.path(base, "GAUGE_CONUS") else file.path(base, "GAUGE_GLB")
+}
 
-cpc_key <- function(year, month, day) {
+cpc_base_file <- function(x) {
+  base <- "PRCP_CU_GAUGE_V1.0%sdeg.lnx."
+  if (x) sprintf(base, "CONUS_0.25") else sprintf(base, "GLB_0.50")
+}
+
+cpc_key <- function(year, month, day, us) {
   sprintf("%s/%s/%s/%s%s%s",
-          cpc_base_ftp(),
+          cpc_base_ftp(us),
           if (year < 2006) "V1.0" else "RT",
           year,
-          "PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.",
+          cpc_base_file(us),
           paste0(year, month, day),
           if (year < 2006) ".gz" else ".RT")
 }
 
-cpc_read <- function(x) {
+cpc_read <- function(x, us) {
   conn <- file(x, "rb")
   on.exit(close(conn))
-  bites <- 360 * 720 * 2
+
+  if (us) {
+    bites <- 120 * 300 * 2
+    lats <- seq(from = 20.125, to = 49.875, by = 0.25)
+    longs <- seq(from = 230.125, to = 304.875, by = 0.25)
+  } else {
+    bites <- 360 * 720 * 2
+    lats <- seq(from = 0.25, to = 89.75, by = 0.5)
+    lats <- c(rev(lats * -1), lats)
+    longs <- seq(from = 0.25, to = 359.75, by = 0.5)
+  }
+
+  # read data
   tmp <- readBin(conn, numeric(), n = bites, size = 4, endian = "little")
   tmp <- tmp[seq_len(bites/2)] * 0.1
-  lats <- seq(from = 0.25, to = 89.75, by = 0.5)
-  lats <- c(rev(lats * -1), lats)
-  longs <- seq(from = 359.75, to = 0.25, by = -0.5)
+
+  # make data.frame
   tibble::as_data_frame(
     stats::setNames(
       cbind(expand.grid(longs, lats), tmp),
