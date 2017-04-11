5797e33b758da182f74629f95d5189cf950d9041
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-11 16:30:39 -0700

    started newrad fxn, not done yet, gitignore some internal notes

---
+++ R/nexrad.R
@@ -0,0 +1,22 @@
+#' NEXRAD II data
+#'
+#' @export
+#' @param x xx
+#' @details
+#' xx
+#' @return data.frame
+#' @examples \dontrun{
+#' f <- tempfile(fileext = ".zip")
+#' nexrad2(2017, 4, 1, 'KAMX', 'KAMX20170401_002500_V06', f)
+#' }
+nexrad2 <- function(year, month, day, station, filename, out, ...) {
+  url <- file.path(nexs3base(), year, add0(month), add0(day), station, filename)
+  res <- GET(url, write_disk(path, TRUE), verbose())
+}
+
+nexs3base <- function() 'https://noaa-nexrad-level2.s3.amazonaws.com'
+# /<Year>/<Month>/<Day>/<NEXRAD Station>/<filename>
+# filename format: GGGGYYYYMMDD_TTTTTT
+# https://noaa-nexrad-level2.s3.amazonaws.com/2017/04/01/KAMX/KAMX20170401_002500_V06
+
+add0 <- function(x) if (nchar(x) == 1) paste0("0", x) else x
