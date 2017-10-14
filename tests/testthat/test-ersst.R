1cd6d2f5f262844e3120f6a8dca7496dfdb5db1c
Jon Calder <jonmcalder@gmail.com>
2017-10-14 16:59:43 +0200

    Add some tests (see #208)

---
+++ tests/testthat/test-ersst.R
@@ -0,0 +1,23 @@
+context("ersst")
+
+test_that("errst works with valid character and numeric input", {
+  skip_on_cran()
+
+  expect_is(ersst(year = 2016, month = 06), "ncdf4")
+  expect_is(ersst(year = 2016, month = 6), "ncdf4")
+  expect_is(ersst(year = "2016", month = 6), "ncdf4")
+  expect_is(ersst(year = "2016", month = "06"), "ncdf4")
+
+})
+
+test_that("errst fails well", {
+
+  expect_error(ersst(year = 2016), "argument \"month\" is missing")
+  expect_error(ersst(month = 10), "argument \"year\" is missing")
+  expect_error(ersst(year = 1853, month = 10), "year must be > 1853")
+  expect_error(ersst(year = 2015, month = 100),
+               "month must be a length 1 or 2 month value")
+  expect_error(ersst(year = 2015, month = 13),
+               "month must be a number between 1 and 12")
+
+})
