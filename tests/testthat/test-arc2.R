ee4697dab3b60bdab3ac77a4d15b7da4987c7acf
Scott Chamberlain <myrmecocystus@gmail.com>
2018-01-08 07:18:15 -0800

    fix test

--- tests/testthat/test-arc2.R
+++ tests/testthat/test-arc2.R
@@ -11,8 +11,7 @@ test_that("arc2 returns the expected output", {
 test_that("arc2 fails with appropriate error messages", {
 
   expect_error(arc2(date = 19830101), "must be of class character, Date")
-  expect_error(arc2(date = "1978-01-01"), "must be between 1979 and 2017")
+  expect_error(arc2(date = "1978-01-01"), "must be between 1979 and 2018")
   expect_error(arc2(date = "1983-13-01"), "must be between 1 and 12")
   expect_error(arc2(date = "1983-01-32"), "must be between 1 and 31")
-
 })
