c82c9ca4d59f3217669c7ff212b0bd0624772061
Scott Chamberlain <myrmecocystus@gmail.com>
2018-02-22 12:20:46 -0800

    fix #258 fixes for autoplot.meteo_coverage fxn, do ungroup on input object first bump dev version and update some ggplot2 variable names to avoid warnings

--- R/meteo-autoplot.R
+++ R/meteo-autoplot.R
@@ -6,6 +6,8 @@
 #' @return A ggplot2 plot
 #' @details see \code{\link{meteo_coverage}} for examples
 autoplot.meteo_coverage <- function(object) {
+  # ungroup
+  object <- dplyr::ungroup(object)
 
   gg <- ggplot2::ggplot(object)
   gg <- gg + ggplot2::geom_segment(data = object,
@@ -13,7 +15,6 @@ autoplot.meteo_coverage <- function(object) {
                                                  xend = ~ stats::reorder(id, start_date),
                                                  y = ~ start_date, yend = ~ end_date))
   gg <- gg + ggplot2::scale_x_discrete(expand = c(0, 0.25))
-  # gg <- gg + ggplot2::scale_y_datetime(expand = c(0, 0))
   gg <- gg + ggplot2::coord_flip()
   gg <- gg + ggplot2::labs(x = NULL, y = NULL, title = "Time coverage by station")
   gg <- gg + ggplot2::theme_bw(base_family = "Arial Narrow")
@@ -36,7 +37,6 @@ autoplot.meteo_coverage <- function(object) {
   gg <- gg + ggplot2::geom_segment(ggplot2::aes_(x = 0, xend = ~ value,
                                                  y = ~ observation, yend = ~ observation,
                                                  group = ~ id))
-  #gg <- gg + ggplot2::scale_x_continuous(labels = percent, limits = c(0, 1))
   gg <- gg + ggplot2::facet_wrap(~id, scales = "free_x")
   gg <- gg + ggplot2::labs(x = NULL, y = NULL, title = "Observation coverage by station")
   gg <- gg + ggplot2::theme_bw(base_family = "Arial Narrow")
@@ -52,11 +52,9 @@ autoplot.meteo_coverage <- function(object) {
                                                                  ggplot2::margin(b = 12)))
   gg <- gg + ggplot2::theme(strip.background = ggplot2::element_blank())
   gg <- gg + ggplot2::theme(strip.text = ggplot2::element_text(hjust = 0))
-  gg <- gg + ggplot2::theme(panel.margin.x = grid::unit(12, "pt"))
-  gg <- gg + ggplot2::theme(panel.margin.y = grid::unit(8, "pt"))
+  gg <- gg + ggplot2::theme(panel.spacing.x = grid::unit(12, "pt"))
+  gg <- gg + ggplot2::theme(panel.spacing.y = grid::unit(8, "pt"))
   gg <- gg + ggplot2::theme(plot.margin = ggplot2::margin(t = 30, b = 5, l = 20, r = 20))
-  ggobs <- gg
 
-  gridExtra::grid.arrange(ggtime, ggobs, ncol=1, heights=c(0.4, 0.6))
-
+  gridExtra::grid.arrange(ggtime, gg, ncol=1, heights=c(0.4, 0.6))
 }
