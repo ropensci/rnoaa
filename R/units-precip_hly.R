54063d88e44f3042bd228e3e7fef18bd27813f3c
Scott Chamberlain <myrmecocystus@gmail.com>
2018-11-30 23:01:35 -0800

    fix non-ascii characters

--- R/units-precip_hly.R
+++ R/units-precip_hly.R
@@ -3,6 +3,6 @@ ncdc_units_precip_hly <- list(
   HPCP = list(
     name = "HPCP", 
     units = "mm_tenths", 
-    description = "The amount of precipitation recorded at the station for the hour ending at the time specified for DATE above given in hundredths of inches or tenths of millimeters depending on user’s specification of standard or metric units. The values 99999 means the data value is missing.  Hours with no precipitation are not shown."
+    description = "The amount of precipitation recorded at the station for the hour ending at the time specified for DATE above given in hundredths of inches or tenths of millimeters depending on user's specification of standard or metric units. The values 99999 means the data value is missing.  Hours with no precipitation are not shown."
   )
 )
