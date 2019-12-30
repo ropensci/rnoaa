e1acf7337d81780d897d5c867ddbfb00ea30ab58
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-30 12:33:22 -0800

    fixes for lcd and isd tests that also cache files

--- tests/testthat/test-isd.R
+++ tests/testthat/test-isd.R
@@ -1,28 +1,25 @@
 context("isd")
 
-# FIXME: uncomment this test block when figure out writing to disk
-# tests alongside caching those files on disk
-
 # delete any cached files
 # isd_dir <- rappdirs::user_cache_dir("rnoaa/isd")
 # unlink(list.files(isd_dir, full.names = TRUE))
 
-# test_that("isd gets data", {
-#   skip_on_cran()
-#   skip_if_government_down()
+test_that("isd gets data", {
+  skip_on_cran()
+  skip_if_government_down()
 
-#   vcr::use_cassette("isd_query", {
-#     data_a <- suppressMessages(isd(usaf = "011490", wban = "99999", year = 1986))
-#     data_b <- suppressMessages(isd(usaf = "011490", wban = "99999", year = 1985))
-#   })
+  vcr::use_cassette("isd_query", {
+    data_a <- suppressMessages(isd(usaf = "011490", wban = "99999", year = 1986))
+    data_b <- suppressMessages(isd(usaf = "011490", wban = "99999", year = 1985))
+  })
 
-#   expect_is(data_a, "tbl_df")
-#   # no longer a df in a list
-#   expect_null(suppressWarnings(data_a$data))
-#   expect_is(data_a$quality, "character")
+  expect_is(data_a, "tbl_df")
+  # no longer a df in a list
+  expect_null(suppressWarnings(data_a$data))
+  expect_is(data_a$quality, "character")
 
-#   expect_lt(NROW(data_a), NROW(data_b))
-# })
+  expect_lt(NROW(data_a), NROW(data_b))
+})
 
 test_that("isd fails well", {
   skip_on_cran()
--- tests/testthat/test-lcd.R
+++ tests/testthat/test-lcd.R
@@ -1,46 +1,43 @@
 context("lcd")
 
-# FIXME: uncomment this test block when figure out writing to disk
-# tests alongside caching those files on disk
-
-# # clean up first
+# clean up first
 # lcd_cache$delete_all()
 
-# test_that("lcd", {
-#   skip_on_cran()
-#   skip_if_government_down()
+test_that("lcd", {
+  skip_on_cran()
+  skip_if_government_down()
 
-#   vcr::use_cassette("lcd_1", {
-#     aa <- lcd(station = "01338099999", year = 2017)
-#   })
+  vcr::use_cassette("lcd_1", {
+    aa <- lcd(station = "01338099999", year = 2017)
+  })
 
-#   expect_is(aa, "tbl_df")
+  expect_is(aa, "tbl_df")
 
-#   expect_type(aa$station, 'integer')
-#   expect_type(aa$date, 'character')
-#   expect_type(aa$latitude, 'double')
-#   expect_type(aa$longitude, 'double')
-#   expect_type(aa$elevation, 'double')
-#   expect_type(aa$hourlysealevelpressure, 'double')
-# })
+  expect_type(aa$station, 'integer')
+  expect_type(aa$date, 'character')
+  expect_type(aa$latitude, 'double')
+  expect_type(aa$longitude, 'double')
+  expect_type(aa$elevation, 'double')
+  expect_type(aa$hourlysealevelpressure, 'double')
+})
 
-# test_that("lcd fails well", {
-#   skip_on_cran()
-#   skip_if_government_down()
+test_that("lcd fails well", {
+  skip_on_cran()
+  skip_if_government_down()
 
-#   # a station/year combination that doesn't exist
-#   vcr::use_cassette("lcd_not_found", {
-#     expect_error(lcd(station = "02413099999", year = "1945"),
-#                  "Not Found", class = "error")
-#   })
+  # a station/year combination that doesn't exist
+  vcr::use_cassette("lcd_not_found", {
+    expect_error(lcd(station = "02413099999", year = "1945"),
+                 "Not Found", class = "error")
+  })
 
-#   # class
-#   expect_error(lcd(5),
-#                "\"year\" is missing, with no default")
-#   expect_error(lcd(5, 5),
-#                "year must be between 1901")
-#   expect_error(lcd(list(1), 5),
-#                "station must be of class")
-#   expect_error(lcd(5, list(1)),
-#                "year must be of class")
-# })
+  # class
+  expect_error(lcd(5),
+               "\"year\" is missing, with no default")
+  expect_error(lcd(5, 5),
+               "year must be between 1901")
+  expect_error(lcd(list(1), 5),
+               "station must be of class")
+  expect_error(lcd(5, list(1)),
+               "year must be of class")
+})
