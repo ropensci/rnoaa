6f1c262d51a010a89bec75ee2478dc5aa27e67d8
Scott Chamberlain <myrmecocystus@gmail.com>
2020-03-04 11:20:36 -0800

    fix #342 product=predictions gives no metadata - just dont adjust times

--- tests/testthat/test-coops.R
+++ tests/testthat/test-coops.R
@@ -12,6 +12,15 @@ test_that("coops works", {
 
   # dimensions
   expect_equal(NCOL(aa$data), 3)
+
+  # works with product="predictions" that used to fail
+  z <- coops_search(station_name = 8551762, 
+    begin_date = 20200501, end_date = 20200531, 
+    datum = "mtl", product = "predictions", time_zone = "lst_ldt")
+  expect_is(z, "list")
+  expect_named(z, "predictions")
+  expect_is(z$predictions, "data.frame")
+  expect_named(z$predictions, c('t', 'v'))
 })
 
 test_that("coops fails well", {
@@ -20,9 +29,7 @@ test_that("coops fails well", {
   skip_on_appveyor()
 
   expect_error(coops_search(), "argument \"product\" is missing, with no default")
-})
 
-test_that("coops fails well", {
   vcr::use_cassette("coops_search_fail_no_data", {  
     expect_error(
       coops_search(station_name = 8775244, begin_date = 20140927, 
@@ -30,9 +37,7 @@ test_that("coops fails well", {
       "No data was found"
     )
   })
-})
 
-test_that("coops fails well", {
   vcr::use_cassette("coops_search_fail_no_datum", {  
     expect_error(
       coops_search(station_name = 8775244, begin_date = 20140927, 
@@ -40,17 +45,13 @@ test_that("coops fails well", {
       "Must specify a datum for water level products"
     )
   })
-})
 
-test_that("coops fails well", {
   expect_error(
     coops_search(station_name = 8724580, begin_date = 20040927,
                  end_date = 20140928, product = "high_low", datum = "stnd"),
     "The maximum duration the NOAA API"
   )
-})
 
-test_that("coops fails well", {
   expect_error(
     coops_search(station_name = "ps0401", begin_date = 20151121,
                  end_date = 20151231, product = "currents"),
