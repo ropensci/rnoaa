b1ca47af7d7e5d179289e540b1c03781a259a55f
Scott Chamberlain <sckott@fastmail.com>
2021-02-15 08:22:15 -0800

    woops, uncomment two test files

--- tests/testthat/test-tornadoes.R
+++ tests/testthat/test-tornadoes.R
@@ -1,10 +1,10 @@
-# context("tornadoes")
+context("tornadoes")
 
-# test_that("tornadoes works", {
-#   skip_on_cran()
-#   skip_on_ci()
-#   skip_if_government_down()
+test_that("tornadoes works", {
+  skip_on_cran()
+  skip_on_ci()
+  skip_if_government_down()
 
-#   torn <- tornadoes()
-#   expect_is(torn, "SpatialLinesDataFrame")
-# })
+  torn <- tornadoes()
+  expect_is(torn, "SpatialLinesDataFrame")
+})
